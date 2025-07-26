601bfc8095a9c250fb0f7662255a88fb0c428839
Damir Arh <damir.arh@gmail.com>
2025-07-26 12:39:23 +0200

    Add project files.

---
+++ Cs14NullConditionalAssignment/BeforeCs14Tests.cs
@@ -0,0 +1,83 @@
+﻿using Shouldly;
+
+namespace Cs14NullConditionalAssignment;
+
+public class BeforeCs14Tests
+{
+    [Test]
+    public void AccessingMemberOfNullThrows()
+    {
+        Report? report = null;
+
+        Should.Throw<NullReferenceException>(() => report.ItemsProcessed);
+    }
+
+    [Test]
+    public void NullConditionalAccessingMemberOfNullDoesNotThrow()
+    {
+        Report? report = null;
+
+        (report?.ItemsProcessed).ShouldBeNull();
+    }
+
+    [Test]
+    public void AccessingElementOfNullThrows()
+    {
+        List<int>? list = null;
+
+        Should.Throw<NullReferenceException>(() => list[0]);
+    }
+
+    [Test]
+    public void NullConditionalAccessingElementOfNullDowNotThrow()
+    {
+        List<int>? list = null;
+
+        (list?[0]).ShouldBeNull();
+    }
+
+    [Test]
+    public void AssignmentToNonNullMemberSucceedsAndExecutes()
+    {
+        Processor processor = new();
+        Report? report = new();
+
+        report.ItemsProcessed = processor.Process();
+
+        report.ShouldNotBeNull();
+        report.ItemsProcessed.ShouldBe(42);
+
+        processor.Processed.ShouldBe(true);
+    }
+
+    [Test]
+    public void AssignmentToNullMemberThrowsButExecutes()
+    {
+        Processor processor = new();
+        Report? report = null;
+
+        Should.Throw<NullReferenceException>(() => report.ItemsProcessed = processor.Process());
+
+        report.ShouldBeNull();
+        (report?.ItemsProcessed).ShouldBeNull();
+
+        processor.Processed.ShouldBe(true);
+    }
+
+    [Test]
+    public void AssignmentInSkippedIfBlockDoesNotThrowOrExecute()
+    {
+        Processor processor = new();
+        Report? report = null;
+
+        if (report != null)
+        {
+            report.ItemsProcessed = processor.Process();
+        }
+
+        report.ShouldBeNull();
+        (report?.ItemsProcessed).ShouldBeNull();
+
+        processor.Processed.ShouldBe(false);
+    }
+}
---
+++ Cs14NullConditionalAssignment/WithCs14Tests.cs
@@ -0,0 +1,48 @@
+﻿using Shouldly;
+
+namespace Cs14NullConditionalAssignment;
+
+public class WithCs14Tests
+{
+    [Test]
+    public void NullConditionalAssignmentToNullMemberDoesNotThrowOrExecute()
+    {
+        Processor processor = new();
+        Report? report = null;
+
+        report?.ItemsProcessed = processor.Process();
+
+        report.ShouldBeNull();
+        (report?.ItemsProcessed).ShouldBeNull();
+
+        processor.Processed.ShouldBe(false);
+    }
+
+    [Test]
+    public void NullConditionalAssignmentToNullElementDoesNotThrowOrExecute()
+    {
+        Processor processor = new();
+        List<int>? list = null;
+
+        list?[0] = processor.Process();
+
+        list.ShouldBeNull();
+        (list?[0]).ShouldBeNull();
+
+        processor.Processed.ShouldBe(false);
+    }
+
+    [Test]
+    public void NullConditionalCompoundAssignmentToNullElementDoesNotThrowOrExecute()
+    {
+        Processor processor = new();
+        Report? report = null;
+
+        report?.ItemsProcessed += processor.Process();
+
+        report.ShouldBeNull();
+        (report?.ItemsProcessed).ShouldBeNull();
+
+        processor.Processed.ShouldBe(false);
+    }
+}
